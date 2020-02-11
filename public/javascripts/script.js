fdcbc6e8bba34458b4d1e12c742565db67758376
any.okulova <any-okulova@yandex.ru>
2020-02-11 20:23:19 +0300

    + items edit

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,3 +1,19 @@
+const sort_value = ['id-asc', 'id-desc', 'text-asc', 'text-desc'];
+const sort_text = ['По возрастанию номера', 'По убыванию номера', 'По алфавиту', 'По алфавиту в обратном порядке'];
+
+createListSortButton();
+
+function createListSortButton() {
+    const sort_buttons = document.createElement('ul');
+    let html = '';
+    for (let i = 0; i < sort_value.length; i++) {
+        html += `<li><input type=\"radio\" name=\"sortType\" 
+                value='${sort_value[i]}'>${sort_text[i]}</li>`;
+    }
+    sort_buttons.innerHTML = html;
+    document.getElementById('sort-form').appendChild(sort_buttons);
+}
+
 const sort_buttons = document.getElementsByName('sortType');
 sort_buttons.forEach(button => {
     button.addEventListener('click', () => {
@@ -6,7 +22,7 @@ sort_buttons.forEach(button => {
     button.checked = sort_type === button.value;
 });
 
-const items = document.querySelectorAll('li');
+const items = document.querySelectorAll('#list-item > li');
 items.forEach(item => {
     const btnEdit = item.querySelector('input[name="btnEdit"]');
     btnEdit.addEventListener('click',() => editItem(item.id));
@@ -15,18 +31,72 @@ items.forEach(item => {
 });
 
 function editItem(id) {
-    // TODO edit
     const item = document.getElementById(id);
-    item.appendChild(document.createElement('input'));
+    item.querySelector('span[name="all"]').style.display = 'none';
+
+    const text = item.querySelector('span[name="text"]').textContent;
+
+    const editArea = document.createElement('span');
+    editArea.setAttribute('name', 'editArea');
+
+    editArea.innerHTML = `<input name="text" value="${text}">`;
+
+    const btnOk =  document.createElement('input');
+    btnOk.type = 'button';
+    btnOk.name = 'btnOk';
+    btnOk.value = 'Ок';
+    btnOk.addEventListener('click', function () {
+        editOk(id);
+    });
+    editArea.appendChild(btnOk);
+
+    const btnCancel =  document.createElement('input');
+    btnCancel.type = 'button';
+    btnCancel.name = 'btnCancel';
+    btnCancel.value = 'Отмена';
+    btnCancel.addEventListener('click', function () {
+        removeEdit(id);
+    });
+    editArea.appendChild(btnCancel);
+
+    item.appendChild(editArea);
+}
+
+function editOk(id) {
+    const item = document.getElementById(id);
+    if (item.querySelector('input[name="text"]').value !== '') {
+        post('/' + id,
+            (content) => item.querySelector('span[name="text"]').textContent
+                = content.text,
+            {id: id, text: item.querySelector('input[name="text"]').value}
+        );
+    }
+    removeEdit(id);
+}
+
+function removeEdit(id) {
+    const item = document.getElementById(id);
+    item.querySelector('span[name="editArea"]').remove();
+    item.querySelector('span[name="all"]').style.display = null;
 }
 
 function deleteItem(id) {
+    post('/delete',
+        () => document.getElementById(id).remove(),
+        {id: id});
+}
+
+function post(url, callback, content) {
     const request = new XMLHttpRequest();
-    request.open('POST', '/delete', true);
+    request.open('POST', url, true);
     request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    request.addEventListener('load', () => {
-        const item = document.getElementById(id);
-        item.remove();
+    request.responseType = 'json';
+    request.addEventListener('load', (data) => {
+        if (data.target.response.error == null) {
+            callback(content, data.target.response);
+        } else {
+            alert('Ошибка: ' + data.target.response.error);
+        }
     });
-    request.send(JSON.stringify({id: id}));
+    request.send(JSON.stringify(content));
 }
--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,10 @@ const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
 
 const connection = mysql.createConnection({
-    host      : 'localhost',
-    user      : 'root',
-    password  : 'root',
-    database  : 'learning_db'
+    host      : process.env.DB_HOST,
+    user      : process.env.DB_USER,
+    password  : process.env.DB_PASSWORD,
+    database  : process.env.DB_DATABASE
 });
 connection.connect();
 
@@ -23,32 +23,35 @@ router.get('/', function (req, res) {
     const sort_args = req.query.sortType.split('-');
     SQL += ' ORDER BY ' + sort_args[0] + ' ' + sort_args[1];
 
-    connection.query(SQL, function (err, rows) {
-        if (err) throw err;
-        res.render('index', {rows: rows, sortType: req.query.sortType});
+    connection.query(SQL, function (error, rows) {
+        if (error) throw error;
+        res.render('index', {title: 'Главная',
+            rows: rows,
+            sortType: req.query.sortType
+        });
     });
 });
 
 router.post('/', function (req, res) {
     connection.query('INSERT INTO data_element(text) VALUES (?)',
-        [req.body.text],function(err, result) {
-            res.redirect('/');
+        [req.body.text],function(error) {
+            res.json({error: error});
         });
 });
 
 router.post('/delete', jsonParser, function (req, res) {
    connection.query('DELETE FROM data_element WHERE id = ?',
-       [req.body.id], function (err, result) {
-            console.log(req.body.id);
-            res.redirect('/');
+       [req.body.id], function (error) {
+            res.json({error: error});
         });
 });
 
 router.post('/:id', function (req, res) {
     connection.query('UPDATE data_element SET text = ? WHERE id = ?',
-        [req.body.text, req.params.id],
-        function () {});
-    res.redirect('/');
+        [req.body.text, req.params.id.replace('item', '')],
+        function (error) {
+            res.json({error: error});
+        });
 });
 
 module.exports = router;
